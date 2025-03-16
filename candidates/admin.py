a77820aa70d8cf0c509a835adbe4d08e2c5bb837
Satyarth Raghuvanshi <srk.satyarth.1@gmail.com>
2025-03-16 07:43:31 +0530

    ♿️ created admin interface for `Candidate` model and configure `candidates` app

---
+++ candidates/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Candidate
+
+
+@admin.register(Candidate)
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ("name", "age", "gender", "email", "phone_number")
+    search_fields = ("name", "email", "phone_number")
+    list_filter = ("gender", "age")
